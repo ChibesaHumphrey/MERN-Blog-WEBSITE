dde6eb390ba8333456c810c18f40e84d0bf09787
ChibesaHumphrey <humphreychibesazm@gmail.com>
2024-03-17 03:45:25 +0000

    created the sign-up page  UI

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,9 +1,128 @@
-import React from 'react'
+import { Button } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>
-      <h1 className=" text-center font-bold text-2xl text-red-600">SignUp</h1>
+    <div className=" min-h-screen mt-20">
+      <div className=" flex flex-col gap-5 max-w-3xl p-4 mx-auto  md:items-center md:flex-row">
+        {/* Left-side Content */}
+        <div className=" flex-1">
+          <p className=" text-sm text-justify flex-1">
+            **Focus** is a powerful mental tool that enables us to channel our
+            attention and energy toward specific tasks or goals.
+            {/* It acts as a
+            magnifying glass, sharpening our abilities and enhancing
+            productivity. When we concentrate on a single objective, we become
+            more efficient, creative, and resilient. By eliminating distractions
+            and maintaining a clear vision, focus allows us to achieve
+            meaningful results and navigate life's challenges with purpose and
+            determination. */}
+          </p>
+        </div>
+
+        {/* Right-side Content */}
+        <div className=" flex-1">
+          <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center">
+              <a
+                href="#"
+                className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+              >
+                <img
+                  className="w-8 h-8 mr-2"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                  alt="logo"
+                />
+                Humphrey's Blog
+              </a>
+              <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Create Account
+                  </h1>
+                  <form className="space-y-4 md:space-y-6" action="#">
+                    <div className="relative z-0 w-full mb-10 group">
+                      <input
+                        type="username"
+                        name="username"
+                        id="username"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required=""
+                        // onChange={(event) => {
+                        //   setusername(event.target.value);
+                        // }}
+                      />
+                      <label
+                        htmlFor="username"
+                        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >
+                        Username
+                      </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-10 group">
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required=""
+                        // onChange={(event) => {
+                        //   setusername(event.target.value);
+                        // }}
+                      />
+                      <label
+                        htmlFor="email"
+                        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >
+                        email
+                      </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-10 group">
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" "
+                        required=""
+                        // onChange={(event) => {
+                        //   setusername(event.target.value);
+                        // }}
+                      />
+                      <label
+                        htmlFor="password"
+                        className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                      >
+                        password
+                      </label>
+                    </div>
+
+                    <Button
+                      type="submit"
+                      gradientDuoTone="purpleToPink"
+                      // className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                      Sign Up
+                    </Button>
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account?{" "}
+                      <Link
+                        to={"/sign-in"}
+                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      >
+                        Sign In here
+                      </Link>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
